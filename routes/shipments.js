0e1e52521ed375000f3ea82b91f38f10fefa8386
Jason <jasoncmcvicar@gmail.com>
2022-07-05 19:50:18 -0500

    initial commit

---
+++ routes/shipments.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const express = require("express");
+const router = new express.Router();
+const { BadRequestError } = require("../expressError");
+const jsonschema = require("jsonschema");
+const shipmentSchema = require("../schemas/shipmentSchema.json");
+
+const { shipProduct } = require("../shipItApi");
+
+/** POST /ship
+ *
+ * VShips an order coming from json body:
+ *   { productId, name, addr, zip }
+ *
+ * Returns { shipped: shipId }
+ */
+
+router.post("/", async function (req, res, next) {
+  const { productId, name, addr, zip } = req.body;
+  const result = jsonschema.validate(
+    req.body, shipmentSchema, {required: true});
+
+  if (!result.valid){
+    const errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const shipId = await shipProduct({ productId, name, addr, zip });
+
+  return res.json({ shipped: shipId})
+
+  //const { productId, name, addr, zip } = req.body;
+  //const shipId = await shipProduct({ productId, name, addr, zip });
+  //return res.json({ shipped: shipId });
+});
+
+
+module.exports = router;
